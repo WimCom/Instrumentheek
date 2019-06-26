b2a95ed620216aa57ce2941be011797df3d96491
WimC <wimcomeyne@hotmail.com>
2019-06-26 13:23:15 +0200

    Take lendings into account when deciding disabled dates

--- Api/product/getDisabledDates.php
+++ Api/product/getDisabledDates.php
@@ -10,16 +10,34 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 include_once '../config/database.php';
 include_once '../objects/product.php';
 include_once '../objects/reservation.php';
+include_once '../objects/product.php';
+include_once '../objects/lending.php';
 include_once '../objects/category.php';
 include_once '../helper/token.php';
 
 
 // get posted data
 $data = json_decode(file_get_contents("php://input"));
-
+$product = new Product();
+$product->ProductID = $data->productID;
 $reservation = new Reservation();
 $reservations = $reservation->getAllReservationsByProduct($data->productID);
+$lending = new Lending();
+$lending->Product = $product;
 $dates = array();
+
+if($lending->getActiveLendingByProduct(1))
+{
+	$start = strtotime($lending->StartDate);
+	$end = strtotime($lending->DueDate);
+	while($start < $end)
+	{
+		array_push($dates, date("Y-m-d",$start));
+		$start = strtotime('+1 day', $start);
+		
+	}
+}
+
 foreach($reservations as $reservation)
 {
 	$start = strtotime($reservation["StartDate"]);
